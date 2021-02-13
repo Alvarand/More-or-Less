82430a6e0863a466de0b7006f5ddf42e792aa163
Shamil <36115557+Alvarand@users.noreply.github.com>
2021-02-13 18:33:05 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import random
 
 
-def check_num(num):
-    return 1 <= num <= 100
+def check_num(check):
+    return 1 <= check <= 100
 
 
 def print_guess():
     print('Guess the number: ', end='')
 
 
+def more_or_less(num, orig):
+    if num > orig:
+        print('Your number is more than my number')
+        print("Let's try again")
+        return False
+    elif num < orig:
+        print('Your number is less than my number')
+        print("Let's try again")
+        return False
+    else:
+        print('My congratulations, you guessed the number')
+        return True
+
+
 print('Welcome to the game MoL.')
-print_guess()
-num = int(input())
+original = random.randint(1, 100)  # original number
 
-if check_num(num):
-    pass
-else:
-    print("This number ain't in range 1-100")
-
+while True:
+    print_guess()
+    guess = int(input())  # guessing number
+    if check_num(guess):
+        if more_or_less(guess, original):
+            break
+    else:
+        print("This number ain't in range 1-100")
+        print("Let's try again")
